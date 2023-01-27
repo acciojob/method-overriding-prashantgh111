0bba0c44cefe95e5d7e9c85056089cbb870b61ee
prashantgh111 <107759833+prashantgh111@users.noreply.github.com>
2023-01-27 23:46:29 +0530

    Solution Overriding

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,30 @@
 package com.driver;
 
+
 public class Main {
-  
+        public static class A {
+
+            public String meth() {
+                return "Invoking method from class A";
+            }
+        }
+        public static class B extends A {
+          // task 4:
+            @Override
+            public String meth(){
+                return "Method is overridden in Extendend class B";
+            }
+        }
+
+    public static void main(String[] args) {
+        // create object og class and call from always main function.
+        // task 3 : is not possible bcoz to call the method of parent class f u already extending it.
+        //but for que we write as it is.
+        B obj = new B();
+        obj.meth();   // if i print then print "Method is overridden in Extendend class B";
+
+        //task 5:
+        B obj1 = new B();
+        obj1.meth();   // if i print then print "Method is overridden in Extendend class B";
+    }
 }
